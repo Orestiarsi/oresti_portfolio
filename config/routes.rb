f8fbbc2fec2d204054531d65c420c840b4b33185
OrestiArsi <orestiarsi@gmail.com>
2017-08-26 20:41:17 -0400

    second commit --connected bio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
  get 'hello/index' => 'hello#index'
+ get 'bio' => 'bio#show'
 
  root 'hello#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
